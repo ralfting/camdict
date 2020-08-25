9f95d77ecd8bec15923347340bbb0815b155d057
Ralph Effting <ralfting@gmail.com>
2020-08-25 08:40:41 -0300

    Add a pretty output Extract peaces of the information in a function

--- app.js
+++ app.js
@@ -1,39 +1,67 @@
 const yargs = require("yargs");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
+const chalk = require("chalk");
+const ora = require("ora");
 
 require("yargs")
   .command({
     command: "dictionary [value]",
     aliases: ["d"],
     desc: "Brings the meaning of the word",
-    handler: async (argv) => {
-      try {
-        const response = await fetch(
-          `https://dictionary.cambridge.org/pt/dicionario/ingles/${argv.value}`
-        );
-
-        const $ = cheerio.load(await response.text());
-        console.log(
-          "Word: " + argv.value + " - " + $(".pos.dpos").first().text()
-        );
-        console.log($(".ddef_d").first().text());
-        console.log("Examples");
-
-        //$(".def-body.ddef_b").each((_idx, el) => console.log($(el).text()));
-        $(".def-body.ddef_b")
-          .first()
-          .find(".examp")
-          .each((_idx, el) => console.log("- " + $(el).text().trim()));
-        //$(".ddef_d").each((_idx, el) => console.log($(el).text()));
-
-        //Object.keys($(".ddef_d")).forEach((text) => {
-        //console.log($[text]);
-        //});
-      } catch (error) {
-        console.error(error);
-      }
-    },
+    handler,
   })
   .demandCommand(1, "You need at least one command before moving on")
   .help().argv;
+
+async function handler(argv) {
+  const { value } = argv;
+
+  if (!value) return console.warn("No word informed");
+
+  try {
+    const spinner = ora("Searching in dictionary...").start();
+
+    const response = await getWordMean(value);
+    const $ = cheerio.load(await response.text());
+
+    spinner.stop();
+
+    headerWord(value, $);
+    getDescription($);
+    getExamples($);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getWordMean(value) {
+  const response = await fetch(
+    `https://dictionary.cambridge.org/dicionario/ingles/${value}`
+  );
+
+  return response;
+}
+
+function headerWord(value, $) {
+  const word = chalk.bgWhite.black(value.toUpperCase());
+  const type = chalk.underline.gray($(".pos.dpos").first().text());
+
+  console.log(`${word} (${type})`);
+}
+
+function getDescription($) {
+  const defination = $(".ddef_d").first().text().trim();
+
+  if (defination) {
+    console.log("Definition:", defination);
+  }
+}
+
+function getExamples($) {
+  const exampleWord = chalk.bgWhite.black("\nExamples");
+  const examples = $(".def-body.ddef_b").first().find(".examp");
+
+  console.log(exampleWord);
+  examples.each((_idx, el) => console.log("- " + $(el).text().trim()));
+}
